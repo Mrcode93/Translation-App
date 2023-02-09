cfdbeb34cb9e61d20fda8c63aaad7c8f37049f4b
Mrcode93 <alhlaqalhlaq667@gmail.com>
2023-02-09 23:59:59 +0300

    v.1.0

---
+++ js/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open("sw.cache").then(function(cache) {
+            return cache.add(
+                "../index.html",
+                "./main.js",
+                "../sass/style.css",
+                "./manifest.webmanifest"
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
